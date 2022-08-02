8f22d4cb4004eb629a259ce373cee88151a278ea
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-02 17:31:46 -0500

    added a emotionsitem component and set it to render all emotions as buttons for now

---
+++ src/components/EmotionsItem/EmotionsItem.jsx
@@ -0,0 +1,13 @@
+
+
+function EmotionsItem ({emotion}){
+
+    return (
+        <div>
+            <button>{emotion.emotion_name}</button>
+        </div>
+    )
+
+};
+
+export default EmotionsItem;
--- src/components/EmotionsList/EmotionsList.jsx
+++ src/components/EmotionsList/EmotionsList.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-
-
+import EmotionsItem from '../EmotionsItem/EmotionsItem';
 
 function EmotionsList (){
 
