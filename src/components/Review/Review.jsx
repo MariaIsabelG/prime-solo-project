51e7dd27bdeb031a433d69c49d552305ced4b24a
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-03 17:50:21 -0500

    added a handlesubmit to the review page

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -6,13 +6,19 @@ function Review (){
     const history = useHistory();
     const student = useSelector( store => store.user)
 
+    function handleSubmit(){
+
+
+
+        history.push('/thanks')
+    };
 
     return (
         <div>
         <h2>{student.full_name}, here are the answers you gave:</h2>
             <h3>Emotion: </h3>
             <h3>Sensation: </h3>
-            <button>Submit</button>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 };
