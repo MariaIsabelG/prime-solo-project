7d8abb9cdc7080b3de8cfea3ba65c769f6b42678
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-04 15:50:18 -0500

    conflicts

--- src/redux/sagas/overview.saga.js
+++ src/redux/sagas/overview.saga.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 function OverviewData (){
     try{
-<<<<<<< HEAD
+
         const response = yield axios.get('/api/students');
         yield put({ type: 'SET_STUDENTS', payload: response.data });
-=======
+
         const response = yield axios.get('/api/states/overview');
         yield put({ type: 'SET_OVERVIEW_DATA', payload: response.data });
->>>>>>> feature-overview-chart
+
     }catch(error) {
         console.log('Error in getting students:', error);
   }
