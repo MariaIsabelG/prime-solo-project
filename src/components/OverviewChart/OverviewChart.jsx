c18fa17b188afa2ea635e24819859861ddd27e67
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-04 13:12:06 -0500

    added a barchart

--- src/components/OverviewChart/OverviewChart.jsx
+++ src/components/OverviewChart/OverviewChart.jsx
@@ -1,12 +1,39 @@
-
-
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+import { Bar } from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
 
 function OverviewChart(){
 
-
+    
+    const data = { 
+        labels: ['Babis', 'Michael', 'Paola'],
+        datasets: [{
+            label: 'Emotions',
+            data: [1, 2, 5],
+            backgroundColor: 'pink',
+            tension: .2,
+            yAxisID: 'emotion',
+            }
+            ,{
+            label: 'Sensation',
+            data: [5, 4, 1],
+            backgroundColor: 'lightblue', 
+            tension: .2,   
+            yAxisID: 'physical',
+            }
+        ],
+            
+        }
 
     return(
-        <h3>Overview Chart</h3>
+        <div>
+            <h3>Overview Chart</h3>
+            <div style={{width: '500px', margin: '0 auto'}}>
+                <Bar data = {data}/>
+            </div> 
+        </div>
+
         
 
     )
