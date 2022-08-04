25d8a81d7ea28f9b3d758a9f06a89fa9f323f537
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-04 10:14:14 -0500

    successfully gathered name

--- src/components/EmotionsViewItem/EmotionsViewItem.jsx
+++ src/components/EmotionsViewItem/EmotionsViewItem.jsx
@@ -1,25 +1,29 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 
 function EmotionsViewItem ({emotion}){
 
-    const [emotionValue, setEmotionValue] = useState({emotion_value: ''})
+    const dispatch = useDispatch();
+    const [emotionValue, setEmotionValue] = useState({emotion_value: '', emotion_name: ''})
 
-    function handleValue (value){
+    function handleValue (value, name){
 
-        setEmotionValue({emotion_value: value})
+        setEmotionValue({emotion_value: value, emotion_name: name})
+        dispatch({
+            type: 'EMOTION_VALUE',
+            payload: emotionValue
+        })
         
     };
 
     console.log('This is emotion value:', emotionValue);
     
 
-
-
-
     return (
         <>
         <span>
-            <button onClick={(event) => handleValue(emotion.emotion_value)}>{emotion.emotion_name}</button>
+            <button onClick={(event) => handleValue(emotion.emotion_value, emotion.emotion_name)}>{emotion.emotion_name}</button>
         </span>
         </>
     )
