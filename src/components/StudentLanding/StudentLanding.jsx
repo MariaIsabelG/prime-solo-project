7870b8f8ca2d4a990b152c50ecf8fe2808d9d8bc
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-05 13:58:23 -0500

    adding abackground

--- src/components/StudentLanding/StudentLanding.jsx
+++ src/components/StudentLanding/StudentLanding.jsx
@@ -17,8 +17,7 @@ function StudentLanding(){
     return (
         <div>
             <h1 className='studentview-title'> Welcome, {student.full_name}!</h1>
-            <center><iframe src="https://giphy.com/embed/E1w0yvMxBIv5M8WkL8" width="200" height="200" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
-            {/* <h3 className='student-text'>Thanks for stopping by. Please take a moment to get comfortable.</h3> */}
+            <center><iframe src="https://giphy.com/embed/RJEBGVo2mrGxsujtAE" width="200" height="200" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
             <h3 className='student-text'>Let's go on self-awareness journey!!!</h3>
             <button className='student-btn' onClick={handleNext}>Start!</button>
             </div>
