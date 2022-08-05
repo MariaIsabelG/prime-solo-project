0d5a3b89fa2ca88d339f20eef4392f740a7a7a62
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-05 14:09:25 -0500

    adding a gif bubble

--- src/components/StudentLanding/StudentLanding.jsx
+++ src/components/StudentLanding/StudentLanding.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import logo from '../Images/';
 
 
 
@@ -18,6 +19,7 @@ function StudentLanding(){
         <div>
             <h1 className='studentview-title'> Welcome, {student.full_name}!</h1>
             <center><iframe src="https://giphy.com/embed/RJEBGVo2mrGxsujtAE" width="200" height="200" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
+            <img src={logo} alt="journey"/>
             <h3 className='student-text'>Let's go on self-awareness journey!!!</h3>
             <button className='student-btn' onClick={handleNext}>Start!</button>
             </div>
