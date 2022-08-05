4cb28374d94487b6e938422dc29e31820fd9da2a
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-05 14:22:05 -0500

    positioning bubble

--- src/components/StudentLanding/StudentLanding.jsx
+++ src/components/StudentLanding/StudentLanding.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import logo from '../Images/';
+import logo from '../Images/journey.gif';
 
 
 
@@ -18,8 +18,12 @@ function StudentLanding(){
     return (
         <div>
             <h1 className='studentview-title'> Welcome, {student.full_name}!</h1>
+            <div className='studentview-gifposition'>
             <center><iframe src="https://giphy.com/embed/RJEBGVo2mrGxsujtAE" width="200" height="200" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
-            <img src={logo} alt="journey"/>
+            </div>
+            <div className='studentview-bubbleposition'>
+            <img  src={logo} width="600" alt="journey"/>
+            </div>
             <h3 className='student-text'>Let's go on self-awareness journey!!!</h3>
             <button className='student-btn' onClick={handleNext}>Start!</button>
             </div>
