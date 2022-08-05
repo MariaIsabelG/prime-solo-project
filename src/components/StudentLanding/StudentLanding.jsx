f627d574b955532ac353c95cb3397d5394511ae4
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-05 10:19:07 -0500

    imported font

--- src/components/StudentLanding/StudentLanding.jsx
+++ src/components/StudentLanding/StudentLanding.jsx
@@ -17,11 +17,12 @@ function StudentLanding(){
     return (
         <div>
             <h1 className='studentview-title'> Welcome, {student.full_name}!</h1>
-            {/* <iframe src="https://giphy.com/embed/E1w0yvMxBIv5M8WkL8" width="300" height="300" frameBorder="0" className="welcome" allowFullScreen></iframe> */}
-            
-            <h3>Thanks for stopping by. Please take a moment to get comfortable.</h3>
-            <h3>When you are ready to begin your check in, click the Next button!</h3>
-            <button onClick={handleNext}>Next</button>
+            <center><iframe src="https://giphy.com/embed/E1w0yvMxBIv5M8WkL8" width="300" height="300" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
+            {/* <h3 className='student-text'>Thanks for stopping by. Please take a moment to get comfortable.</h3> */}
+            <h3 className='student-text'>Let's go on self-awareness journey!!!</h3>
+            <div className='student-btn'>
+                <button  onClick={handleNext}>Start!</button>
+            </div>
         </div>
     )
 
