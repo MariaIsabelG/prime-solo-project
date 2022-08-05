c7ac9ce6135a7713b4b521c3a5c7170ead611a31
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-05 10:59:55 -0500

    edited gif border

--- src/components/StudentLanding/StudentLanding.jsx
+++ src/components/StudentLanding/StudentLanding.jsx
@@ -17,7 +17,7 @@ function StudentLanding(){
     return (
         <div>
             <h1 className='studentview-title'> Welcome, {student.full_name}!</h1>
-            <center><iframe src="https://giphy.com/embed/E1w0yvMxBIv5M8WkL8" width="300" height="300" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
+            <center><iframe src="https://giphy.com/embed/E1w0yvMxBIv5M8WkL8" width="200" height="200" frameBorder="0" className="student-gif" allowFullScreen></iframe></center>
             {/* <h3 className='student-text'>Thanks for stopping by. Please take a moment to get comfortable.</h3> */}
             <h3 className='student-text'>Let's go on self-awareness journey!!!</h3>
             <button className='student-btn' onClick={handleNext}>Start!</button>
